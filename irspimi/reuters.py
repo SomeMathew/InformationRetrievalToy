42df2a2d12d1207caa4d53559e4861c593eff1fb
SomeMatthew <someMathew@outlook.com>
2018-10-20 23:54:40 -0400

    Speed improvement to the Reuters Corpus token streamer.
    The stream of tokens now properly uses deque for the buffers instead
    of list for quicker left pop.

--- irspimi/reuters.py
+++ irspimi/reuters.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from nltk import word_tokenize, sent_tokenize
 from itertools import chain
 from dict_compression import Compression
-from collections import namedtuple
+from collections import namedtuple, deque
 
 
 class ReutersDocument:
@@ -21,6 +21,11 @@ class ReutersDocument:
             mknote_tag.extract()
 
     def get_tokens(self):
+        """Retrieves a buffer of tokens for this document.
+
+        :return: Queue of tokens for this document
+        :rtype: deque
+        """
         if not self._soup:
             raise ReutersCorpusException("Could not parse the reuters SGML")
         # remove new line or carriage return escape chars
@@ -28,8 +33,8 @@ class ReutersDocument:
         # tokenize by words using the default NLTK word tokenizer
         # and remove stray punctuation
         sentences = sent_tokenize(text)
-        tokens = [tok for tok in chain.from_iterable([word_tokenize(sent) for sent in sentences]) if
-                  tok not in list(string.punctuation)]
+        tokens = deque(tok for tok in chain.from_iterable([word_tokenize(sent) for sent in sentences]) if
+                       tok not in list(string.punctuation))
         return tokens
 
 
@@ -44,7 +49,7 @@ class ReutersCorpus:
         self._current_docid = None
         self._current_pos = 0
         self._compression = compression
-        # self._current_doc = None
+        self.docid_list = []
 
     def __iter__(self):
         return self
@@ -55,10 +60,11 @@ class ReutersCorpus:
             if not nextdoc:
                 raise StopIteration
             self._current_docid = nextdoc.docid
+            self.docid_list.append(nextdoc.docid)
             self._current_tokens = nextdoc.get_tokens()
             self._current_pos = 0
         if self._current_tokens:
-            next_token = self._current_tokens.pop(0)
+            next_token = self._current_tokens.popleft()
             self._current_pos += 1
             if self._compression:
                 next_token = self._compression.compress(next_token)
@@ -71,7 +77,7 @@ class ReutersCorpus:
     def _next_doc(self):
         if not self._docs:
             self._docs = self._fetch_next_chunk()
-        return self._docs.pop(0) if self._docs else None
+        return self._docs.popleft() if self._docs else None
 
     def has_next_doc(self):
         if not self._docs:
@@ -79,14 +85,19 @@ class ReutersCorpus:
         return self._docs is not None and len(self._docs) != 0
 
     def _fetch_next_chunk(self):
-        reuters_docs = []
+        """Retrieves the next chunk of documents for the Reuters Corpus
+
+        :return: Buffer of ReutersDocument
+        :rtype: deque
+        """
+        reuters_docs = deque()
         current_file = None
         while self._files and not current_file:
             filename = self._files.pop(0)
             try:
                 current_file = open(filename, 'r', errors='ignore')
                 soup = BeautifulSoup(current_file, "html.parser")
-                reuters_docs = [ReutersDocument(doc) for doc in soup.find_all("reuters")]
+                reuters_docs.extend(ReutersDocument(doc) for doc in soup.find_all("reuters"))
             except IOError:
                 print("Could not find {}".format(filename))
         return reuters_docs
